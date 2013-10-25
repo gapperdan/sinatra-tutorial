88d6701e01ecc79cc71d0c244b7ce9a880fb08ef
Danny Tizon <dtizon@gmail.com>
2013-10-24 23:55:08 -0700

    updated fake-school-web

--- fake-school-web/main-controller.rb
+++ fake-school-web/main-controller.rb
@@ -3,11 +3,39 @@ require 'sinatra'
 #http://localhost:4567
 
 get '/' do
-  erb :form
+  erb :login
 end
 
 post '/' do
-  "You are logged in as: '#{params[:userid]}'"
+
+  VALID_USERS = ['Obi Wan','Yoda','Anakin']
+  VALID_PASSWORD = 'force'
+
+  puts params[:userid]
+
+  if VALID_USERS.include? params[:userid] and VALID_PASSWORD == params[:password]
+
+  #if params[:userid] == 'Yoda' || 'Anakin' || 'Obi Wan'
+    #redirect to('/master?userid='+params[:userid])
+    redirect to '/authorized?userid='+params[:userid]
+  else
+    redirect to '/unauthorized'
+  end
+
+end
+
+get '/master' do
+  "You are a Jedi Master"
+end
+
+get '/authorized' do
+  "Valid login"
+  "Userid="+params[:userid]
+
+end
+
+get '/unauthorized' do
+  "Invalid login"
 end
 
 #404 catcher
