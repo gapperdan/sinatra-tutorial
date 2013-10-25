435b680da408fa2613e9eb6a8df0452fee309e12
Danny Tizon <dtizon@gmail.com>
2013-10-25 09:52:49 -0700

    updates

--- fake-school-web/main-controller.rb
+++ fake-school-web/main-controller.rb
@@ -2,41 +2,66 @@ require 'sinatra'
 
 #http://localhost:4567
 
+JEDI_MASTER = 'yoda'
+JEDI_KNIGHT = 'obi wan'
+JEDI_PADAWAN = 'anakin'
+
+INVALID_LOGIN = 'The force is not with you, please try again.'
+
 get '/' do
   erb :login
 end
 
 post '/' do
 
-  VALID_USERS = ['Obi Wan','Yoda','Anakin']
+  VALID_USERS = ['obi wan','yoda','anakin']
   VALID_PASSWORD = 'force'
 
-  puts params[:userid]
+  puts params[:username].downcase
 
-  if VALID_USERS.include? params[:userid] and VALID_PASSWORD == params[:password]
-
-  #if params[:userid] == 'Yoda' || 'Anakin' || 'Obi Wan'
-    #redirect to('/master?userid='+params[:userid])
-    redirect to '/authorized?userid='+params[:userid]
+  if VALID_USERS.include? params[:username].downcase and VALID_PASSWORD == params[:password].downcase
+    redirect to '/authorized?username='+params[:username].downcase
   else
     redirect to '/unauthorized'
   end
 
 end
 
-get '/master' do
-  "You are a Jedi Master"
-end
-
 get '/authorized' do
-  "Valid login"
-  "Userid="+params[:userid]
+  #this_user = params[:username]
+  #"Valid login"
+  #"Userid="+params[:username]
 
+  case params[:username]
+    when JEDI_MASTER
+      redirect to '/master?username='+params[:username]
+    when JEDI_KNIGHT
+      redirect to '/knight?username='+params[:username]
+    when JEDI_PADAWAN
+      redirect to '/padawan?username='+params[:username]
+  end
 end
 
 get '/unauthorized' do
-  "Invalid login"
+  @errmsg = INVALID_LOGIN
+  erb :login
 end
+
+get '/master' do
+  @username = params[:username].capitalize
+  erb :master
+end
+
+get '/knight' do
+  @username = params[:username].capitalize
+  erb :knight
+end
+
+get '/padawan' do
+  @username = params[:username].capitalize
+  erb :padawan
+end
+
 
 #404 catcher
 not_found do
